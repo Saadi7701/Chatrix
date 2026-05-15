81adaf9acf806ff9d6fc2000f9b32fe4ace279bd
Saadi7701 <mrsami1330@gmail.com>
2026-05-15 23:26:59 +0500

    Fix: Enhanced ICE configuration with multi-STUN failover for mobile stability

--- client/src/components/CallOverlay.tsx
+++ client/src/components/CallOverlay.tsx
@@ -8,7 +8,12 @@ const ICE_SERVERS = {
    iceServers: [
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
+      { urls: 'stun:stun2.l.google.com:19302' },
+      { urls: 'stun:stun3.l.google.com:19302' },
+      { urls: 'stun:stun4.l.google.com:19302' },
+      { urls: 'stun:stun.services.mozilla.com' },
    ],
+   iceCandidatePoolSize: 10,
 };
 
 const CallOverlay = () => {
